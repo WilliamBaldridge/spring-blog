fd0ff15650d4c7f876ae334a953d2793ee9ad0b0
WilliamBaldridge <baldridge.william09@gmail.com>
2021-04-28 11:41:16 -0500

    Spring controllers exercise complete

--- src/main/java/com/codeup/SpringBlog/controllers/HelloController.java
+++ src/main/java/com/codeup/SpringBlog/controllers/HelloController.java
@@ -2,9 +2,7 @@ package com.codeup.SpringBlog.controllers;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
@@ -22,6 +20,9 @@ public class HelloController {
         return "Hello " + name + "!";
     }
 
-
-
+    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable int number) {
+        return number + " plus one is " + (number + 1) + "!";
+    }
 }
