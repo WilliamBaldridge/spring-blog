6726feed3b907cda6518c14c0cd6de3aa3f740ec
WilliamBaldridge <baldridge.william09@gmail.com>
2021-04-28 10:10:44 -0500

    feat: fixed Spring initializer file name error and first controller setup

--- src/main/java/com/codeup/SpringBlog/SpringBlogApplication.java
+++ src/main/java/com/codeup/SpringBlog/SpringBlogApplication.java
@@ -1,4 +1,4 @@
-package com.codeup.springBlog;
+package com.codeup.SpringBlog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/codeup/SpringBlog/SpringBlogApplicationTests.java
+++ src/test/java/com/codeup/SpringBlog/SpringBlogApplicationTests.java
@@ -1,4 +1,4 @@
-package com.codeup.springBlog;
+package com.codeup.SpringBlog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
