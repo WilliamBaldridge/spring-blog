2626f2dba8a433c5f6c121346d3567ccceb877b0
WilliamBaldridge <baldridge.william09@gmail.com>
2021-05-11 12:19:40 -0500

    feat: adding integration testing

--- src/test/java/com/codeup/SpringBlog/SpringBlogApplicationTests.java
+++ src/test/java/com/codeup/SpringBlog/SpringBlogApplicationTests.java
@@ -3,11 +3,24 @@ package com.codeup.SpringBlog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.servlet.http.HttpSession;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.security.config.http.MatcherType.mvc;
+
 @SpringBootTest
 class SpringBlogApplicationTests {
 
+	private HttpSession httpSession;
+
 	@Test
 	void contextLoads() {
+
 	}
 
+
+	@Test
+	public void testIfUserSessionIsActive() throws Exception {
+
+	}
 }
