3eff415e0ddf4480113cea90b6e60ac3c63e0c16
WilliamBaldridge <baldridge.william09@gmail.com>
2021-05-12 09:35:58 -0500

    dokku deployment with digi ocean

--- src/test/java/com/codeup/SpringBlog/SpringBlogApplicationTests.java
+++ src/test/java/com/codeup/SpringBlog/SpringBlogApplicationTests.java
@@ -1,24 +1,24 @@
-package com.codeup.SpringBlog;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.servlet.http.HttpSession;
-
-
-@SpringBootTest
-class SpringBlogApplicationTests {
-
-	private HttpSession httpSession;
-
-	@Test
-	void contextLoads() {
-
-	}
-
-
-	@Test
-	public void testIfUserSessionIsActive() throws Exception {
-
-	}
-}
+//package com.codeup.SpringBlog;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import javax.servlet.http.HttpSession;
+//
+//
+//@SpringBootTest
+//class SpringBlogApplicationTests {
+//
+//	private HttpSession httpSession;
+//
+//	@Test
+//	void contextLoads() {
+//
+//	}
+//
+//
+//	@Test
+//	public void testIfUserSessionIsActive() throws Exception {
+//
+//	}
+//}
